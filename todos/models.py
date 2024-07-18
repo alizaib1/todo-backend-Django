8dad5f3d0c12d7e9a8b7289ee991dbe53865d561
alizaib1 <alizaibkhan6666@gmail.com>
2024-07-19 00:11:07 +0500

    commit message

---
+++ todos/models.py
@@ -0,0 +1,11 @@
+# todos/models.py
+
+from django.db import models
+
+class Todo(models.Model):
+    text = models.CharField(max_length=200)
+    is_complete = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.text
